94dfd4a17453a77121c4c4daf6f6419282dbe6b1
vasskob <vasskob@gmail.com>
2017-04-18 13:13:02 +0300

    Add dummy markers to realm db. Add icons for markers.

--- app/src/main/java/com/task/vasskob/googlemapsrealm/MapsActivity.java
+++ app/src/main/java/com/task/vasskob/googlemapsrealm/MapsActivity.java
@@ -6,17 +6,27 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.task.vasskob.googlemapsrealm.model.Marker;
+import com.task.vasskob.googlemapsrealm.realm.RealmController;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        realm = RealmController.with(this).getRealm();
     }
 
 
@@ -40,20 +52,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
+            googleMap.setMyLocationEnabled(true);
         } else {
-// Show rationale and request permission.
+            Log.d("onMapReady","Permission deny");
+            // Show rationale and request permission.
         }
 
+        // Add dummy markers to db
+        setRealmData();
+        RealmController.with(this).refresh();
+
+        LatLng markerLatLng;
+        String markerTitle;
+        BitmapDescriptor markerIcon;
+        RealmResults<Marker> markers = RealmController.with(this).getMarkers();
+
+        for (Marker marker : markers) {
+            markerLatLng = new LatLng(marker.getLatitude(), marker.getLongitude());
+            markerTitle = marker.getLabel();
+            markerIcon = BitmapDescriptorFactory.fromResource(manageMarkerIcon(marker.getIcon()));
+            googleMap.addMarker(new MarkerOptions().position(markerLatLng).title(markerTitle).icon(markerIcon));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerLatLng));
+        }
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    private void setRealmData() {
+
+        ArrayList<Marker> markers = new ArrayList<>();
+
+        Marker marker = new Marker();
+        marker.setId(1 + System.currentTimeMillis());
+        marker.setLabel("Barcelona");
+        marker.setIcon("icon1");
+        marker.setLatitude(41.23d);
+        marker.setLongitude(2.09d);
+        markers.add(marker);
+
+        marker = new Marker();
+        marker.setId(2 + System.currentTimeMillis());
+        marker.setLabel("Paris");
+        marker.setIcon("icon2");
+        marker.setLatitude(48.48d);
+        marker.setLongitude(2.20d);
+        markers.add(marker);
+
+        marker = new Marker();
+        marker.setId(3 + System.currentTimeMillis());
+        marker.setLabel("Rome");
+        marker.setIcon("icon3");
+        marker.setLatitude(41.54d);
+        marker.setLongitude(12.27d);
+        markers.add(marker);
+
+        marker = new Marker();
+        marker.setId(4 + System.currentTimeMillis());
+        marker.setLabel("Tokyo");
+        marker.setIcon("icon4");
+        marker.setLatitude(35.40d);
+        marker.setLongitude(139.45d);
+        markers.add(marker);
+
+        marker = new Marker();
+        marker.setId(5 + System.currentTimeMillis());
+        marker.setLabel("Sydney");
+        marker.setIcon("icon1");
+        marker.setLatitude(-34d);
+        marker.setLongitude(151d);
+        markers.add(marker);
+
+        for (Marker m : markers) {
+            // Persist your data easily
+            realm.beginTransaction();
+            realm.copyToRealm(m);
+            realm.commitTransaction();
+        }
+
+        //    Prefs.with(this).setPreLoad(true);
+
+    }
+
+    private int manageMarkerIcon(String markerIcon) {
+        switch (markerIcon) {
+            case "icon1":
+                return R.drawable.icon1;
+            case "icon2":
+                return R.drawable.icon2;
+            case "icon3":
+                return R.drawable.icon3;
+            case "icon4":
+                return R.drawable.icon4;
+            default:
+                return R.drawable.icon_default;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/task/vasskob/googlemapsrealm/model/Marker.java
@@ -0,0 +1,54 @@
+package com.task.vasskob.googlemapsrealm.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Marker extends RealmObject {
+
+    @PrimaryKey
+    private long id;
+    private String label;
+    private String icon;
+    private Double latitude;
+    private Double longitude;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
