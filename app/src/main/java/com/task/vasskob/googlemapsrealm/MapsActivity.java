2781014e418a8d575372d287325914537f9818d8
vasskob <vasskob@gmail.com>
2017-04-18 19:22:29 +0300

    Bug fixing with marker icon select.

--- app/src/main/java/com/task/vasskob/googlemapsrealm/MapsActivity.java
+++ app/src/main/java/com/task/vasskob/googlemapsrealm/MapsActivity.java
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
 import com.task.vasskob.googlemapsrealm.app.Prefs;
 import com.task.vasskob.googlemapsrealm.model.Marker;
 import com.task.vasskob.googlemapsrealm.realm.RealmController;
@@ -37,6 +38,7 @@ import io.realm.RealmResults;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String TAG = MapsActivity.class.getSimpleName();
+    public static final String MISSING_TITLE_WARN = "Entry not saved, missing title";
     private Realm realm;
     private String mTitle;
     private GoogleMap mMap;
@@ -56,7 +58,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             view.setSelected(value);
         }
     };
-    private int mCurrentPosition;
+    private int selectedImageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,20 +70,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         realm = RealmController.with(this).getRealm();
+
+
+        //Log.d(TAG, "onCreate:  icon1 id= " + R.drawable.icon1+"/n icon2 id= "+ R.drawable.icon2+  "/n icon3 id= "+ R.drawable.icon3 +"/n icon4 id= "+ R.drawable.icon4);
+
     }
 
     @OnClick({R.id.icon1, R.id.icon2, R.id.icon3, R.id.icon4})
     public void onIconClick(View view) {
-        Log.d(TAG, "onTabClick: id: " + view.getId());
-        selectTab(getTabPosition(view.getId()));
-        mCurrentPosition=view.getId();
+        Log.d(TAG, "onIconClick: id= " + view.getId());
+        selectIcon(getTabPosition(view.getId()));
+        selectedImageBtn = view.getId();
     }
 
-    private void selectTab(int position) {
+    private void selectIcon(int position) {
         ButterKnife.apply(mIcons, SELECT, false);
         mIcons.get(position - 1).setSelected(true);
 
-        Log.d(TAG, "selectTab: Tab selected  =  " + position);
+        //Log.d(TAG, "selectIcon: Tab selected  =  " + position);
     }
 
     private int getTabPosition(int tabID) {
@@ -120,7 +126,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         RealmController.with(this).refresh();
-
         showMarkersOnMap(googleMap);
 
         googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
@@ -142,7 +147,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             markerTitle = marker.getLabel();
             markerIcon = BitmapDescriptorFactory.fromResource(manageMarkerIcon(marker.getIcon()));
             googleMap.addMarker(new MarkerOptions().position(markerLatLng).title(markerTitle).icon(markerIcon));
-
             //googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerLatLng));
         }
     }
@@ -151,6 +155,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
         final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+        ButterKnife.bind(this, dialogView);
         dialogBuilder.setView(dialogView);
 
         final EditText edt = (EditText) dialogView.findViewById(R.id.edit1);
@@ -164,12 +169,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 marker.setLabel(mTitle);
                 marker.setLatitude(latLng.latitude);
                 marker.setLongitude(latLng.longitude);
-                marker.setIcon("icon3");
+                marker.setIcon(manageReverseMarkerIcon(selectedImageBtn));
 
                 if (edt.getText().toString().equals("")) {
-                    Toast.makeText(MapsActivity.this, "Entry not saved, missing title", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MapsActivity.this, MISSING_TITLE_WARN, Toast.LENGTH_LONG).show();
                 } else {
-                    mMap.addMarker(new MarkerOptions().position(latLng).title(mTitle));
+                    BitmapDescriptor markerIcon1 = BitmapDescriptorFactory.fromResource(manageMarkerIcon(manageReverseMarkerIcon(selectedImageBtn)));
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(mTitle).icon(markerIcon1));
                     writeToRealm(marker);
                 }
             }
@@ -258,4 +264,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    private String manageReverseMarkerIcon(int id) {
+        switch (id) {
+            case R.id.icon1:
+                return "icon1";
+            case R.id.icon2:
+                return "icon2";
+            case R.id.icon3:
+                return "icon3";
+            case R.id.icon4:
+                return "icon4";
+            default:
+                return "icon5";
+        }
+    }
 }
